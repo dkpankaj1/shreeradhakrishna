b4bcedd3c2fe14ac86f8f1636c496bd7d2e74ef3
Dipankar <DipankarPankaj38@gmail.com>
2025-05-20 03:15:14 +0000

    fixes

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -94,7 +94,8 @@ class CustomerController extends Controller
         try {
             $customerData = Customer::create($customer);
 
-            $template = WaTemplate::where(['template_id' => 'welcome_srke'])->first();
+            // $template = WaTemplate::where(['template_id' => 'welcome_srke'])->first();
+             $template = WaTemplate::where(['template_id' => 'new_welcome'])->first();
             $whatsappService = new WhatsAppService();
             $whatsappService->sendNormalText($customerData->phone, $template->template_id);
 
